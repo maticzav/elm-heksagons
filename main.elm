d243e12cad88a26a6621cde956a95b61a9f8b757
matomatic <maticzavadlal@Matic.local>
2016-02-25 16:02:48 +0100

    Nesting functions

--- main.elm
+++ main.elm
@@ -1,2 +1,33 @@
+import Graphics.Element exposing (show)
+import String
 
-1 :: 2 :: []
+fortyTwo : Int
+fortyTwo =
+   42
+
+names : List String
+names =
+   ["Alice", "Foo", "a"]
+
+book : { title: String, author: String, pages: Int }
+book =
+   { title = "foo", author = "bar", pages = 100}
+
+longesNameLength : List String -> Maybe Int
+longesNameLength names =
+   List.maximum (List.map String.length names)
+
+longesName : List String -> Maybe String
+longesName names =
+   List.head (List.sortBy String.length names)
+
+
+flippedComparison a b =
+    case compare a b of
+      LT -> GT
+      EQ -> EQ
+      GT -> LT
+
+
+main =
+   show [List.sortWith flippedComparison (String.length names)]
