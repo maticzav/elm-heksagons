f62db2cb862ade0372a659ef5961a5643c603d43
maticzav <maticzavadlal@Matic-2.local>
2016-03-12 11:32:24 +0100

    Heksagons!
    Narjena mreza!

--- main.elm
+++ main.elm
@@ -1,33 +1,55 @@
-import Graphics.Element exposing (show)
-import String
+import Color exposing (..)
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
 
-fortyTwo : Int
-fortyTwo =
-   42
+r = 40
+margin = 1
 
-names : List String
-names =
-   ["Alice", "Foo", "a"]
+hexagons =
+  [ (0, 0)
+  , (1, 0)
+  , (1, 1)
+  , (2, 0)
+  , (2, -1)
+  , (3, 0)
+  , (3, 1)
+  , (2, -3)
+  , (3, 4)
+  , (-2, 1)
+  , (-5,4)
+  , (-2, -3)
+  , (-3, 4)
+  , (-1, 1)
+  , (-2,4)
 
-book : { title: String, author: String, pages: Int }
-book =
-   { title = "foo", author = "bar", pages = 100}
+  ]
 
-longesNameLength : List String -> Maybe Int
-longesNameLength names =
-   List.maximum (List.map String.length names)
+draw_hexagon : Float -> (Int, Int) -> Form
+draw_hexagon r (x, y) =
+  let
+    pos_x = (toFloat x) * (r + margin) * (1 + cos (pi/3))
+    pos_y = (toFloat (y - (x % 2) * (x//abs x) + (y % 2) * (y//abs y))) * (r + margin) * cos (pi/6)
+  in
+    ngon 6 r
+    |> filled niceColor
+    |> move (pos_x, pos_y)
 
-longesName : List String -> Maybe String
-longesName names =
-   List.head (List.sortBy String.length names)
+draw_hexagons : List (Int, Int) -> List Form
+draw_hexagons hexagons =
+  List.map (draw_hexagon r) hexagons
 
+draw : List Form -> Element
+draw =
+  collage 600 600
 
-flippedComparison a b =
-    case compare a b of
-      LT -> GT
-      EQ -> EQ
-      GT -> LT
+main : Element
+main = draw (draw_hexagons hexagons)
 
-
-main =
-   show [List.sortWith flippedComparison (String.length names)]
+niceColor : Color
+niceColor =
+   let
+      r = 100
+      g = 200
+      b = 255
+   in
+      rgba r g b 0.6
